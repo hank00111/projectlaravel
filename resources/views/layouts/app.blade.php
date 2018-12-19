48ae929db868a1f0ab0f8a1809d7778ff695b01f
hank00111 <awp11612@gmail.com>
2018-12-19 21:30:07 +0800

    APP

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="https://fonts.gstatic.com">
+
+    <!-- Styles -->
+    <link href="{{ asset('css/comm.css') }}" rel="stylesheet">
+
+</head>
+<body>
+    <div id="app">
+        @include('layouts.nav')
+
+        <main>
+
+            <div class="container">
+
+                <div class="main_1">
+                    @yield('content')
+                </div>
+
+            </div>
+
+        </main>
+ 
+    </div>
+
+    <div class="jumbotron text-center" style="margin-bottom:0;padding:3rem 2rem">
+        <p style="color:#fff">©2018 Project HANK</p>
+    </div>
+
+</body>
+</html>
